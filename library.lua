a269f64b1a8c1c92e5047bc33957c23238f0f99f
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-03-21 14:42:16 -0500

    Create library.lua

---
+++ library.lua
@@ -0,0 +1,35 @@
+local suc,err = pcall(function()
+function gets(service)
+    return game:GetService(service)
+end
+function ts(string)
+    return tostring(string)
+end
+function add(num,num2)
+    return num+num2
+end
+function multiply(num,num2)
+    return num*num2
+end
+function subtract(num,num2)
+    return num-num2
+end
+function divide(num,num2)
+    return num/num2
+end
+local plrs = gets("Players")
+local repstorage = gets("ReplicatedStorage")
+
+local plr = plrs.LocalPlayer
+local plrname = ts(plr)
+local plrdisplay = ts(plr.DisplayName)
+local char = plr.Character
+local hum = char.Humanoid
+local hrp = char.HumanoidRootPart
+local ws = hum.WalkSpeed
+local jp = hum.JumpPower
+end)
+
+if not suc then
+    warn("Init lib failure:",err)
+end
